02dd1fe0174631a6768c2738d5273bcdfd557773
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2021-08-04 14:17:47 -0500

    Add files via upload

--- calculadora.js
+++ calculadora.js
@@ -2,8 +2,9 @@ function calcularHuella(){
     let luz = parseInt($("#luz").val())
     let gas = parseInt($("#gas").val())
     let gasolina = parseInt($("#gasolina").val())
-    if (luz && gas && gasolina){
-      let resultado=luz+gas+gasolina
-      $("#resultado").val(resultado+' ton CO2')
+    let familiares = parseInt($("#familiares").val())
+    if (luz && gas && gasolina && familiares){
+      let resultado=((luz*0.39)+(gas*0.20)+(gasolina*2.32))/familiares
+      $("#resultado").val(resultado+' Kg CO2')
     }
   }
