7bb9858d0ae64bf4280b99c5367ac16caaea0730
Nicolas Garzon Vasquez <43526116+nicolasgarzon15@users.noreply.github.com>
2021-08-04 13:39:40 -0500

    Delete Demo directory

--- Demo/calculadora.js
+++
@@ -1,9 +0,0 @@
-function calcularHuella(){
-    let luz = parseInt($("#luz").val())
-    let gas = parseInt($("#gas").val())
-    let gasolina = parseInt($("#gasolina").val())
-    if (luz && gas && gasolina){
-      let resultado=luz+gas+gasolina
-      $("#resultado").val(resultado+' ton CO2')
-    }
-  }
